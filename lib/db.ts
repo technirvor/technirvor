b56bb39a79ee414af6bc911e41ee585530cbebf8
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2025-07-19 05:39:39 +0600

    remove hardcoded MongoDB URI for improved security

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://technirvorbd:nEvRjn1ldLW52DGn@production.tdwry4g.mongodb.net/systemnull"
+const MONGODB_URI = process.env.MONGODB_URI || "";
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local")
